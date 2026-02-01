a059c6ecc8dd4abb7f3f804afc254138da72269d
Nafis5115 <72686804+Nafis5115@users.noreply.github.com>
2026-02-01 10:33:47 +0600

    added auth layout

--- src/routes/router.jsx
+++ src/routes/router.jsx
@@ -3,6 +3,9 @@ import App from "../App";
 import RootLayout from "../layouts/RootLayout";
 import HomePage from "../pages/Home/Home/HomePage";
 import Coverage from "../pages/Coverage/Coverage";
+import AuthLayout from "../layouts/AuthLayout";
+import Login from "../pages/Auth/Login/Login";
+import Register from "../pages/Auth/Register/Register";
 
 const router = createBrowserRouter([
   {
@@ -16,6 +19,20 @@ const router = createBrowserRouter([
       {
         path: "coverage",
         Component: Coverage,
+      },
+    ],
+  },
+  {
+    path: "/",
+    Component: AuthLayout,
+    children: [
+      {
+        path: "login",
+        Component: Login,
+      },
+      {
+        path: "register",
+        Component: Register,
       },
     ],
   },
