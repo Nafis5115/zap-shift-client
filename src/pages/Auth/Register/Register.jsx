97e0a443c7bbdc1744cb4ae124b44d15b4adefc9
Nafis5115 <72686804+Nafis5115@users.noreply.github.com>
2026-02-01 20:00:10 +0600

    setup firebase

--- src/pages/Auth/Register/Register.jsx
+++ src/pages/Auth/Register/Register.jsx
@@ -1,7 +1,48 @@
 import React from "react";
+import { useForm } from "react-hook-form";
 
 const Register = () => {
-  return <div>This is register</div>;
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+  const onSubmit = (data) => {
+    console.log(data);
+  };
+  return (
+    <form onSubmit={handleSubmit(onSubmit)}>
+      <fieldset className="fieldset">
+        <label className="label">Email</label>
+        <input
+          type="email"
+          {...register("email", { required: true })}
+          className="input"
+          placeholder="Email"
+        />
+        {errors.email?.type === "required" && (
+          <p className="text-red-500">Email is required</p>
+        )}
+        <label className="label">Password</label>
+        <input
+          type="password"
+          {...register("password", { required: true, minLength: 6 })}
+          className="input"
+          placeholder="Password"
+        />
+        {errors.password?.type === "required" && (
+          <p className="text-red-500">Password is required.</p>
+        )}
+        {errors.password?.type === "minLength" && (
+          <p className="text-red-500">Password must be 6 character.</p>
+        )}
+        <div>
+          <a className="link link-hover">Forgot password?</a>
+        </div>
+        <button className="btn btn-neutral mt-4">Login</button>
+      </fieldset>
+    </form>
+  );
 };
 
 export default Register;
