42f3ec748ac9c39dbc08947ad36235f81604d8af
Nafis5115 <72686804+Nafis5115@users.noreply.github.com>
2026-02-15 10:29:40 +0600

    my-parcels ongoing

--- src/pages/Dashboard/MyParcels/MyParcels.jsx
+++ src/pages/Dashboard/MyParcels/MyParcels.jsx
@@ -1,7 +1,49 @@
+import { useQuery } from "@tanstack/react-query";
 import React from "react";
+import useAuth from "../../../hooks/useAuth";
+import useAxiosSecure from "../../../hooks/useAxiosSecure";
 
 const MyParcels = () => {
-  return <div>All of my parcels</div>;
+  const { user } = useAuth();
+  const axiosSecure = useAxiosSecure();
+  const { data: parcels = [], isLoading } = useQuery({
+    queryKey: ["MyParcels", user?.email],
+    queryFn: async () => {
+      const res = await axiosSecure.get(`/parcels?email=${user?.email}`);
+      return res.data;
+    },
+  });
+  return (
+    <div>
+      {isLoading ? (
+        <h1>Loading...</h1>
+      ) : (
+        <div className="overflow-x-auto">
+          <table className="table">
+            {/* head */}
+            <thead>
+              <tr>
+                <th></th>
+                <th>Name</th>
+                <th>Receiver Email</th>
+                <th>Cost</th>
+              </tr>
+            </thead>
+            <tbody>
+              {parcels.map((parcel, index) => (
+                <tr>
+                  <th>{index + 1}</th>
+                  <td>{parcel.parcelName}</td>
+                  <td>{parcel.receiverEmail}</td>
+                  <td>{parcel.deliveryCost}</td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </div>
+      )}
+    </div>
+  );
 };
 
 export default MyParcels;
