572b1aea1fef58b034d7bf4011e927e58b9b5015
Nafis5115 <72686804+Nafis5115@users.noreply.github.com>
2026-03-02 11:01:37 +0600

    completed user search

--- src/pages/Dashboard/UserManagement/UserManagement.jsx
+++ src/pages/Dashboard/UserManagement/UserManagement.jsx
@@ -1,14 +1,15 @@
 import { useQuery } from "@tanstack/react-query";
-import React from "react";
+import React, { useState } from "react";
 import useAxiosSecure from "../../../hooks/useAxiosSecure";
 import Swal from "sweetalert2";
 
 const UserManagement = () => {
   const axiosSecure = useAxiosSecure();
+  const [searchText, setSearchText] = useState("");
   const { data: users = [], refetch } = useQuery({
-    queryKey: ["users"],
+    queryKey: ["users", searchText],
     queryFn: async () => {
-      const res = await axiosSecure.get(`/get-users`);
+      const res = await axiosSecure.get(`/get-users?search=${searchText}`);
       return res.data;
     },
   });
@@ -40,6 +41,32 @@ const UserManagement = () => {
   };
   return (
     <div className="overflow-x-auto">
+      <label className="input">
+        <svg
+          className="h-[1em] opacity-50"
+          xmlns="http://www.w3.org/2000/svg"
+          viewBox="0 0 24 24"
+        >
+          <g
+            strokeLinejoin="round"
+            strokeLinecap="round"
+            strokeWidth="2.5"
+            fill="none"
+            stroke="currentColor"
+          >
+            <circle cx="11" cy="11" r="8"></circle>
+            <path d="m21 21-4.3-4.3"></path>
+          </g>
+        </svg>
+        <input
+          onChange={(e) => setSearchText(e.target.value)}
+          type="search"
+          className="grow"
+          placeholder="Search"
+        />
+        <kbd className="kbd kbd-sm">⌘</kbd>
+        <kbd className="kbd kbd-sm">K</kbd>
+      </label>
       <table className="table table-zebra">
         <thead>
           <tr>
